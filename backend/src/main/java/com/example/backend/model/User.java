d4cc08f91c9b0eb185d196dbca28f86c8e3df306
bhaskar <baruahbhaskar10@gmail.com>
2026-02-07 19:07:51 +0530

    removed some backend

--- backend/src/main/java/com/example/backend/model/User.java
+++ backend/src/main/java/com/example/backend/model/User.java
@@ -1,11 +1,16 @@
 package com.example.backend.model;
 
-import jakarta.persistence.*;
-import jakarta.validation.Valid;
+import org.springframework.stereotype.Component;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Component;
 
 @Component
 @AllArgsConstructor
